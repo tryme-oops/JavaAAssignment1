7ba6f71d90fd7aebbf927a9209ffb88a06c71f3d
tryme-oops <78734373+tryme-oops@users.noreply.github.com>
2021-04-14 15:22:25 +0530

    Create question:3.java

---
+++ question:3.java
@@ -0,0 +1,34 @@
+package binrayjump;
+
+import java.util.Scanner;
+
+public class binaryJump {
+
+	public static void main(String[] args) {
+		int j=0;
+		int[] arr=new int[100];
+		
+		Scanner input = new Scanner(System.in);
+		
+		for(int i=0; i<arr.length;i++)
+		{
+			arr[i]=input.nextInt();
+		}
+		
+		for(int i=0; i<arr.length-1;)
+		{
+			if(arr[i]==0 && arr[i+2] == 0)
+			{
+				j++;
+				i=i+2;
+			}
+			else if (arr[i] == 0 && arr[i+1]==0)
+			{
+				j++;
+				i++;
+			}
+		}
+		
+		System.out.println(j);
+	}
+}
